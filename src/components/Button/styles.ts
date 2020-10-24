a83fb2a2d8032fdc90005fe45e6fff03b6c643fb
Tatilon <tatilonroberto@hotmail.com>
2020-10-23 23:21:40 -0300

    isolando componentsv2

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,10 +1,24 @@
 import styled from 'styled-components';
 import { shade } from 'polished';
-import signInBackground from '../../assets/sign-in-background.png';
-
-
 
 export const Container = styled.button`
+
+
+
+        background:  #ff9000;
+        height: 56px;
+        border-radius: 10px;
+        border: 2px solid #232129;
+        padding: 0 16px;
+        color: #312e38;
+        width: 100%;
+        font-weight: 500;
+        margin-top: 16px;
+        transition: background-color 0.2s;
+
+        &:hover{
+            background: ${shade(0.2, '#ff9000')}
+        }
 
 
 
